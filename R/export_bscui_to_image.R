40fd548747f1d4c15db7da5e6c528b7882455745
Patrice Godard <patrice.godard@gmail.com>
2023-12-19 11:53:25 +0100

    further parameters

--- R/export_bscui_to_image.R
+++ R/export_bscui_to_image.R
@@ -4,21 +4,34 @@
 #' @param widget a [`bscui`] object
 #' @param file name of output file. Should end with an image file type
 #' (.png, .jpg, .jpeg, or .webp) or .pdf.
-#' @param delay Time to wait before taking screenshot, in seconds.
-#' Sometimes a longer delay is needed for all assets to display properly.
-#' @param quiet If TRUE (default), status updates via console messages are
+#' @param selector ([webshot2::webshot()]) one or more CSS selectors specifying
+#' a DOM element to set the clipping rectangle to (default: ".bscui").
+#' @param zoom ([webshot2::webshot()]) If TRUE (default),
+#' status updates via console messages are suppressed.
+#' @param quiet ([webshot2::webshot()]) a number specifying the zoom factor.
+#' @param ... additional parameters for [webshot2::webshot()]
 #' suppressed.
 #'
 #' @seealso [webshot2::webshot()]
 #'
 #' @export
 #'
-export_bscui_to_image <- function(widget, file, delay=0.2, quiet = TRUE){
+export_bscui_to_image <- function(
+   widget, file,
+   selector=".bscui",
+   zoom = 1,
+   quiet = TRUE,
+   ...
+){
    html_file <- tempfile(fileext = ".html")
    on.exit(file.remove(html_file))
    htmlwidgets::saveWidget(widget, html_file)
    html_file <- normalizePath(html_file, winslash="/")
    invisible(webshot2::webshot(
-      html_file, file=file, selector=".bscui", delay=delay, quiet = quiet
+      html_file, file=file,
+      selector=selector,
+      zoom = zoom,
+      quiet = quiet,
+      ...
    ))
 }
